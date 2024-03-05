2eb3945e56a955d1ca0302204b131cff952fe4ba
Nardos-Amakele <150350627+Nardos-Amakele@users.noreply.github.com>
2024-03-05 17:08:52 +0300

    13 exercise

---
+++ LAB-2-ASYNC.dart
@@ -0,0 +1,10 @@
+Future<String> fetchData() {
+  return Future.delayed(Duration(seconds: 2), () => 'Data fetched successfully');
+}
+
+void main() {
+  print('Fetching data...');
+  fetchData().then((data) {
+    print('Data: $data');
+  });
+}
